1b3232f19a0ab260cd66c08d31e0c37a203e226f
wolfsinem <43369471+wolfsinem@users.noreply.github.com>
2019-11-18 17:41:06 +0100

    Update MatrixTranspositie.cpp

--- MatrixTranspositie.cpp
+++ MatrixTranspositie.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 using namespace std;
 
+// g++ -std=c++17 MatrixTranspositie.cpp -o main
+// ./main
+
 void print(vector<vector<int>> x) {
   for (int i = 0; i < x.size(); i++) {
     for (int j = 0; j < x[i].size(); j++) {
@@ -20,7 +23,7 @@ void MatrixTranspositie(vector<vector<int>> &x) {
   }
 }
 
-main() {
+int main() {
   vector<vector<int>> x;
   x = {{0, 1, 2}, {3, 4, 5}, {6, 7, 9}};
   x = {{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
